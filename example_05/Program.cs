4e3cf80562555213cbb3ae54fcc93e56145e6fe0
gmakc-094423 <gmakc-094423@yandex.ru>
2022-04-11 21:00:38 +0300

    Семинар 01. первые две задачи

---
+++ example_05/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Ввести имя пользователя: ");
+string userName = Console.ReadLine();
+
+if (userName == "Маша")
+{
+  Console.WriteLine("Да ты же Маша!, привет!");
+}
+else 
+{
+  Console.WriteLine("Ты не Маша :( . Но всё равно привет, " + userName);
+}
---
+++ seminar_01/Program.cs
@@ -0,0 +1,73 @@
+﻿Console.Write("Введи первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введи второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int square = numberA * numberA;
+
+if (square == numberB)
+{
+Console.Write("Да");
+}
+else 
+{
+Console.WriteLine("Нет");
+}
+
+
+Console.WriteLine("Введи число дня недели: ");
+int numberС = Convert.ToInt32(Console.ReadLine());
+
+if (numberС == 1)
+{
+  Console.Write("Понедельник");
+}
+
+else 
+{
+if (numberС == 2)
+{
+  Console.Write("Вторник");
+}
+
+else 
+{
+if (numberС == 3)
+{
+  Console.Write("Среда");
+}
+
+else 
+{
+if (numberС == 4)
+{
+  Console.Write("Четверг");
+}
+
+else 
+{
+if (numberС == 5)
+{
+  Console.Write("Пятница");
+}
+
+else 
+{
+if (numberС == 6)
+{
+  Console.Write("Суббота");
+}
+
+else 
+{
+if (numberС == 7)
+{
+  Console.Write("Воскресенье");
+}
+}
+}
+}
+}
+}
+}
